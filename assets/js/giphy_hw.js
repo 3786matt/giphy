816e4f3db4c469f819b6c69d8b6c4ce18d404797
Matthew Mandel <matthewmandel@Matthew-Mandels-MacBook-Pro.local>
2016-09-07 18:28:17 -0400

    gihpy

--- assets/js/giphy_hw.js
+++ assets/js/giphy_hw.js
@@ -16,26 +16,42 @@ var bands = ['Megadeth', 'Metallica', 'Ozzy Osbourne', 'Black Label Society', 'S
       console.log(response);
 
       var gifs = response.data;
+
+      var container= $('.images');
+        container.empty();
      
       $.each(gifs, function(key, value){
-        var container= $('.images');
-       
+
+        var newDiv = $('<div>');
+
+
+
         gif=value;
      
         original_url=gif.images.fixed_height_still.url;
+        originalAnimated_url1=gif.images.fixed_height.url;
         original_url1=gif.rating;
         
         newImage=$('<img>');
         // newRate=$('<div/>');
         
         newImage.attr('src', original_url);
+        newImage.attr('data-still', original_url);
+        newImage.attr('data-animated', originalAnimated_url1);
+        newImage.attr('data-state', 'still');
+        newImage.addClass('gif');
         // newRate.attr('txt', original_url1);
         newImage.attr('rating', original_url1);
+        newRating=$('<p>');
+        newRating.text('Rating: ' + original_url1);
+        // container.append($('<br>'),'Rating: ' + original_url1, $('<br>'), newImage);
         
-        container.append('Rating: ' + original_url1, newImage);
-        // container.append(newImage);
+        newDiv.append(newRating);
+        newDiv.append(newImage);
+        newDiv.attr('style', 'display:inline-block')
+        container.append(newDiv);
 
-        
+//cmd option f;
 
         // container.appendTo()
         // $('.images').append(newImage.rating);
@@ -69,17 +85,60 @@ var bands = ['Megadeth', 'Metallica', 'Ozzy Osbourne', 'Black Label Society', 'S
     }
 }
 
-$('#extraMetal').on('click', function(){
+$('#extraMetal').on('click', function(event){
 
-var metal = $('#metal-inject').val().trim();
-bands.push(metal);
-divMaker();
-return false;
+  // event.preventDefault();
+  var metalInput = $('#metal-inject').val().trim();
+// if('#metal-inject').val().trim().contains('.bands'){
+  if(($.inArray(metalInput, bands) >= 0) || metalInput === ""){
+    console.log('invalid input');
+  // return false;
+  }else{
+    console.log('new stuff yo')
+    bands.push(metalInput);
+    divMaker();
+    $('#metal-inject').val('');  
+  }
 
-})
+  return false;
+
+// if('#metalCollect'.contains(val()){
+//   return false;
+// }
+
+
+  // );     
+
+});
+
+// function clear() {
+//   $('#metalCollect').empty();
+// };
+
+// $('.band').on('click', '#metalCollect', empty());
 
 $(document).on('click', '.band', metalOutput);
 
 divMaker();
+
+// $('#jsonStuff').on('click', empty());
+
+$(document).on('click', ".gif", function(){
+  if($(this).attr('data-state')==='still') {
+    $(this).attr('data-state', 'animated');
+    $(this).attr('src', $(this).attr('data-animated'));
+  }else{
+    $(this).attr('src', $(this).attr('data-still'));
+    $(this).attr('data-state', 'still');
+  }
+
+
+  
+
+
+  
+});
+
+
 
 // metalOutput();
